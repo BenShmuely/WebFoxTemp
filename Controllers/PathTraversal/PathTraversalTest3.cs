6d3feff6c14413e2fa826f31dbee81be2caa22e1
BenShmuely <78843342+BenShmuely@users.noreply.github.com>
2021-03-08 09:48:28 +0200

    Add files via upload

---
+++ Controllers/PathTraversal/PathTraversalTest3.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebFox.Controllers.PathTraversal
+{
+    public class PathTraversalTest3
+    {
+        static readonly string rootFolder = @"C:\Temp\Data\"; 
+        
+        [HttpGet("{userInput}")]
+        static void Test(string userInput)    
+        {
+            var fullPath = Path.Combine(rootFolder, userInput);
+            var reader = new StreamReader(fullPath);
+            reader.ReadToEnd();
+        }
+    }
+}
